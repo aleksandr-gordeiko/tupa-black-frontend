6bb4d72d8e10654d7c939f4a75a393410ee62e72
Aleksandr Gordeiko <gordeiko.ae@niuitmo.ru>
2023-01-04 15:37:21 +0300

    Migrated to new backend address

--- src/App.svelte
+++ src/App.svelte
@@ -23,7 +23,7 @@
       redirect: 'follow',
     };
 
-    const url = 'https://black.simpatiya.ga/api';
+    const url = 'https://black.graminit.ru/api';
 
     fetch(url, requestOptions)
       .then((t) => t.blob().then((b) => {
